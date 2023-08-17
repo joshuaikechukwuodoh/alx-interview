606a66f29ec40ab6388c52ed095c75051027fa3d
Odoh Joshua ikechukwu <113645159+joshuaikechukwuodoh@users.noreply.github.com>
2023-08-17 16:06:53 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,38 @@
 #!/usr/bin/python3
-"""
-log parsing
-"""
-
 import sys
-import re
+import signal
 
+def print_stats(file_size, status_counts):
+    print("File size: {:d}".format(file_size))
+    for code, count in sorted(status_counts.items()):
+        print("{:d}: {:d}".format(code, count))
 
-def output(log: dict) -> None:
-    """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
-
-
-if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
-
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+def main():
+    file_size = 0
+    status_counts = {}
 
     try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
+        for i, line in enumerate(sys.stdin, start=1):
+            parts = line.split()
+            if len(parts) != 9:
+                continue
 
-                # File size
-                log["file_size"] += file_size
+            try:
+                status_code = int(parts[7])
+                size = int(parts[8])
+            except (ValueError, IndexError):
+                continue
 
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
+            file_size += size
+            status_counts[status_code] = status_counts.get(status_code, 0) + 1
 
-                if (line_count % 10 == 0):
-                    output(log)
-    finally:
-        output(log)
+            if i % 10 == 0:
+                print_stats(file_size, status_counts)
+
+    except KeyboardInterrupt:
+        pass
+
+    print_stats(file_size, status_counts)
+
+if __name__ == "__main__":
+    main()
